c985d9a0ade8cda82ef7ee9d3232ce2cc4d31827
bb <whynot.byoul@gmail.com>
2023-03-23 07:47:41 +0900

    coin api 가져오기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoding] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((reponse) => reponse.json())
+      .then((json) => {
+        setCoins(json);
+        setLoding(false);
+      });
+  }, []);
+  //[]비워둘 경우, 페이지 열릴때 딱 1번 실행
+
+  return (
+    <div className="App">
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading</strong>
+      ) : (
+        <div>
+          {" "}
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+              </option>
+            ))}
+            {/* react 에서 map 은 element 에 key를 꼭 부여해주어야함(유니크하게) */}
+          </select>
+          <input type="text" />
+          <button>change</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;

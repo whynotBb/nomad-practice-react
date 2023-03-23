220c993c6722117c567ddbd37a21a6684aa67bab
bb <whynot.byoul@gmail.com>
2023-03-24 07:44:50 +0900

    coin tracker 완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import { useEffect, useState } from "react";
+
 function App() {
   const [loading, setLoding] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState("");
+  const [money, setMoney] = useState("");
+  const [click, setClick] = useState(false);
+  const [account, setAccount] = useState("");
+  const onChange = (event) => {
+    setCost(event.target.value);
+  };
+  const inputValue = (event) => {
+    setMoney(event.target.value);
+  };
+  const onClick = () => {
+    setAccount(money / cost);
+    setClick(true);
+    console.log(account);
+  };
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((reponse) => reponse.json())
@@ -19,17 +36,20 @@ function App() {
         <strong>Loading</strong>
       ) : (
         <div>
-          {" "}
-          <select>
+          <select onChange={onChange}>
             {coins.map((coin) => (
-              <option key={coin.id}>
+              <option key={coin.id} value={coin.quotes.USD.price}>
                 {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
               </option>
             ))}
             {/* react 에서 map 은 element 에 key를 꼭 부여해주어야함(유니크하게) */}
           </select>
-          <input type="text" />
-          <button>change</button>
+          <div>
+            <input type="text" placeholder="USD" onChange={inputValue} />
+            <button onClick={onClick}>change</button>
+
+            {click ? <p>You can get {account}coin</p> : null}
+          </div>
         </div>
       )}
     </div>

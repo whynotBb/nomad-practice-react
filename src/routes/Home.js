dc48b893036817e237b6d6314892b5d1509d2cd1
bb <whynot.byoul@gmail.com>
2023-03-28 07:27:29 +0900

    add styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -22,7 +23,7 @@ function Home() {
       {loading ? (
         <h1>Loading</h1>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
